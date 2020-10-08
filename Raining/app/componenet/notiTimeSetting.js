df357f18f7b49a8e93e517536f6b0865ab36084c
xiah <xiah0526@gmail.com>
2020-10-08 16:45:01 +0900

    BackgroundJob Task period 동작 확인 및 Test 진행을 계속한다

--- Raining/app/componenet/notiTimeSetting.js
+++ Raining/app/componenet/notiTimeSetting.js
@@ -10,11 +10,10 @@ const NotiTimeSetting = () => {
   const [date, setDate] = useState(Date.now());
   const [mode, setMode] = useState('time');
   const [show, setShow] = useState(true);
-
+  let message = `지금부터 매일 ${date} 시간이 되면 알람이 울립니다!`;
   const backgroundJob = {
     jobKey: 'today',
     job: () => {
-      PushNotification.cancelAllLocalNotifications();
       Geolocation.getCurrentPosition(
         (position) => {
           const key = '';
@@ -30,7 +29,6 @@ const NotiTimeSetting = () => {
               // console.log('locationKey: ', value);
               locationKey = value.Details.CanonicalLocationKey;
             });
-          // 337169
           fetch(
             `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${key}&language=ko&details=true&metric=false`,
           )
@@ -39,31 +37,23 @@ const NotiTimeSetting = () => {
               rainProbability = value.DailyForecasts[0].Day.RainProbability;
             });
 
+          const rainMessages = [
+            '강수 확률이 50%이상이에요! 우산을 꼭 챙기세요',
+            '오늘 비가 올수도 있어요, 우산 필수!',
+            '우산을 꼭 들고 출근하세요!',
+          ];
+
+          const noneMessages = [
+            '오늘은 우산이 필요 없어요',
+            '우산 없는 화창한 하루 되어요!',
+          ];
+
           if (rainProbability >= 50) {
-            const messages = [
-              '강수 확률이 50%이상이에요! 우산을 꼭 챙기세요',
-              '오늘 비가 올수도 있어요, 우산 필수!',
-              '우산을 꼭 들고 출근하세요!',
-            ];
-            const message =
-              messages[Math.floor(Math.random() * messages.length)];
-
-            PushNotification.localNotificationSchedule({
-              /* Android Only Properties */
-              vibrate: true,
-              vibration: 300,
-              priority: 'hight',
-              visibility: 'public',
-              importance: 'hight',
-
-              /* iOS and Android properties */
-              message, // (required)
-              playSound: true,
-              number: 1,
-              actions: '["OK"]',
-              repeatType: 'day',
-              date: new Date(date),
-            });
+            message =
+              rainMessages[Math.floor(Math.random() * rainMessages.length)];
+          } else {
+            message =
+              noneMessages[Math.floor(Math.random() * noneMessages.length)];
           }
         },
         (error) => {
@@ -80,7 +70,9 @@ const NotiTimeSetting = () => {
     jobKey: 'today',
     allowExecutionInForeground: true,
     allowWhileIdle: true,
-    period: 216000000,
+    // period: 216000000, //6시간
+    // period: 108000000, //3시간
+    period: 900000, //15분
   };
 
   BackgroundJob.schedule(backgroundSchedule)
@@ -88,7 +80,30 @@ const NotiTimeSetting = () => {
     .catch((err) => console.err(err));
 
   const onChange = (event) => {
+    // if(event.type == "set") {
+    //     //ok button clicked
+    // } else {
+    //     //cancel button clicked
+    // }
     setDate(event.nativeEvent.timestamp);
+    PushNotification.cancelAllLocalNotifications();
+    PushNotification.localNotificationSchedule({
+      /* Android Only Properties */
+      vibrate: true,
+      vibration: 300,
+      priority: 'hight',
+      visibility: 'public',
+      importance: 'hight',
+
+      /* iOS and Android properties */
+      message, // (required)
+      playSound: true,
+      number: 1,
+      actions: '["OK"]',
+      repeatType: 'day',
+      date: new Date(date),
+    });
+
     BackHandler.exitApp();
   };
 
