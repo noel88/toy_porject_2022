9f73ef27cdc411d8628725c085c764fdb484f771
Xiah <xiah0526@gmail.com>
2021-03-02 11:01:57 +0900

    Mapper를 Bean으로 등록하여 사용하도록 설정한다

---
+++ mycat/src/main/java/com/ntoday/mycat/Application.java
@@ -0,0 +1,21 @@
+package com.ntoday.mycat;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
