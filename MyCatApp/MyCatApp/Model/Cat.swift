61ccfe0010d59ea999c14c4a9a3c7ccca3ee8970
xiah <xiah0526@gmail.com>
2021-03-05 22:23:16 +0900

    IOS 기본 프로젝트를 셋업한다

---
+++ MyCatApp/MyCatApp/Model/Cat.swift
@@ -0,0 +1,19 @@
+//
+//  Cat.swift
+//  MyCatApp
+//
+//  Created by N on 2021/03/05.
+//
+
+import Foundation
+
+
+struct Cat: Codable {
+    var name: String
+    var gender: String
+    var age: String
+    var breeds: String
+    var birthday: String
+    var isNeutering: Bool
+    var isDead: Bool
+}
