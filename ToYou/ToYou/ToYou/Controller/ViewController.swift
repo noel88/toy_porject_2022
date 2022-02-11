cd0ac28143c8b82943026ba01193c3f2dd7d114f
Xiah <xiah0526@gmail.com>
2022-02-11 17:51:35 +0900

    XCode 기본 프로젝트를 생성한다

---
+++ ToYou/ToYou/ToYou/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToYou
+//
+//  Created by Xiah Lee on 2022/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
